2e45869b39b403aabb60ff16c0a96e5dd836d764
roniantoniius <aantoniusron@gmail.com>
2024-08-31 13:30:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,5 @@ async def shutdown():
     db_engine.dispose()
     print("Database Marine Product Disconnected")
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
